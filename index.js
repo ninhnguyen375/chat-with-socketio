47e1e2ed3c0df46daeadd64c6b388d08493b8a13
ninhnguyen375 <ninhnguyen375@gmail.com>
2019-01-08 22:22:58 +0700

    ninh

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var mongoose = require("mongoose")
+mongoose.connect(
+  "mongodb://ninhnguyen375:ninhnguyen3755@ds123662.mlab.com:23662/appchatdb",
+  {useNewUrlParser: true}
+)
+let db = mongoose.connection
+
+const messSchema = new mongoose.Schema({
+  message: String
+});
+
+const Mess = mongoose.model('Mess', messSchema, 'Mess');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', async function(socket){
+  socket.on('chat message', async function(msg){
+    await Mess.insertMany({message: msg});
+    let newMess = await Mess.find();
+    io.emit('chat message', newMess);
   });
 });
 
